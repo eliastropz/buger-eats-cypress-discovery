9733c959eeffdd6d1f4b8a3e9c0c284f3c5d72b6
Elias Gonçalves de Siqueira <elias.siqueira@ms.senac.br>
2022-01-30 22:08:37 -0400

    Cypress discovery

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,25 @@
+
+
+describe('home page', ()=>{
+
+    before(function() {
+        cy.log('Tudo aqui é executado uma única vez ANTES de TODOS os casos de teste')
+    })
+
+    beforeEach(function() {
+        cy.log('Tudo aqui é executado sempre ANTES de CADA caso de teste')
+    })
+
+    after(function() {
+        cy.log('Tudo aqui é executado uma única vez DEPOIS de TODOS os casos de teste')
+    })
+
+    afterEach(function() {
+        cy.log('Tudo aqui é executado sempre DEPOIS de CADA caso de teste')
+    })
+    it('app deve estar online', ()=>{
+        cy.viewport(1440, 1080)        
+        cy.visit('/')
+        cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
+    })
+})
